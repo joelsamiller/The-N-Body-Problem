cfc8e3a130a259de7f4db10e57769ad7d91be86a
Joel Miller <joelsamiller@gmail.com>
2024-04-28 22:43:33 +0100

    refactor: Implement forward Euler solver

--- src/the_n_body_problem/objects/system.py
+++ src/the_n_body_problem/objects/system.py
@@ -9,40 +9,36 @@ from the_n_body_problem.physics import constants
 class System:
     def __init__(self, bodies: list[Body]):
         self.bodies = bodies
+        self.n = len(bodies)
 
     def solve(self, time: np.ndarray) -> None:
-        # Reshape positions and velocities into a single 1D list for ODE solver
-        pos = sum([list(b.pos) for b in self.bodies], [])
-        vel = sum([list(b.vel) for b in self.bodies], [])
-        pos_vel = pos + vel
+        # Create position, velocity and u arrays
+        pos = np.stack([b.pos for b in self.bodies])
+        vel = np.stack([b.vel for b in self.bodies])
         # Create a list of all body masses for solver
         mass = np.array([b.mass for b in self.bodies])
+        dt = int(time[-1] / len(time))
 
-        # Define function to solve
-        def newton(y, t, mass):
-            n = len(mass)
-            # Reshape positions and velocites into a n x 3 array
-            pos = np.array(y[0 : int(len(y) / 2)]).reshape((-1, 3))
-            vel = np.array(y[int(len(y) / 2) : :]).reshape((-1, 3))
+        u = np.zeros((len(time) + 1, 2, self.n, 3))
+        u[0] = np.stack([pos, vel])
+        for k, _ in enumerate(time):
+            # Split positions and velocites into n x 3 arrays
+            pos = u[k][0]
+            vel = u[k][1]
             # Calculate distance from each object to every other object
             norm_dist = distance.cdist(pos, pos)  # ||x_i - x_j||
             vector_dist = pos[:, None, :] - pos  # x_i - x_j
             # Calculate coordinate invariate part of field matrix
-            a_mag = -constants.G * mass * norm_dist ** -3  # Gm/||r||^3
+            a_mag = -constants.G * mass * norm_dist**-3  # Gm/||r||^3
             np.fill_diagonal(a_mag, 0)  # Set acceleration to 0 for i=j
             # Multiply by displacement vector for each body
-            acc = np.array([a_mag[i] @ vector_dist[i] for i in range(n)])
-            # Flatten velocity and acceleration arrays and join as one list for output
-            out = vel.ravel().tolist() + acc.ravel().tolist()
-            return out
+            acc = np.array([a_mag[i] @ vector_dist[i] for i in range(self.n)])
+            # Join velocities and accelerations and return
+            vel += dt * acc
+            pos += dt * vel
+            u[k + 1] = np.stack([pos, vel])
 
-        # Solve ODE
-        solution = odeint(newton, pos_vel, time, (mass,))
         # Assign solutions for each body to its class
-        pos = [
-            [solution[i][3 * j : 3 * (j + 1)] for i in range(len(time))]
-            for j in range(len(self.bodies))
-        ]
         for i, b in enumerate(self.bodies):
-            b.path = pos[i]
-            b.pos = pos[i][-1]
+            b.path = u[:, 0, i, :]
+            b.pos = u[:, 0, -1, :]
