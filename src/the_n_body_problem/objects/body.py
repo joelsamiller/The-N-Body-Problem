212d74c8fb37f879ceae25d7f601a86c20227bae
Joel Miller <joelsamiller@gmail.com>
2024-11-30 22:06:59 +0000

    fix: Coerce values to float

--- src/the_n_body_problem/objects/body.py
+++ src/the_n_body_problem/objects/body.py
@@ -3,9 +3,9 @@ import numpy as np
 
 class Body:
     def __init__(self, mass: float, pos: list[float], vel: list[float], colour: str):
-        self.mass = mass
-        self.pos = np.array(pos)
-        self.vel = np.array(vel)
+        self.mass = float(mass)
+        self.pos = np.array(pos).astype(float)
+        self.vel = np.array(vel).astype(float)
         self.colour = colour
         self.path = []
 
